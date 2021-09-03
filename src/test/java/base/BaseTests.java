4da500cc550d852b00aca08f1db6e978a04f265b
MarwanMokh <marwanmokhtar2612@gmail.com>
2021-09-03 12:05:07 +0200

    The First Commit for project Structure

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTests {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        goHome();
+    }
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
+    }
+
+}
---
+++ src/test/java/firstTest/HomePageTest.java
@@ -0,0 +1,13 @@
+package firstTest;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class HomePageTest extends BaseTests {
+
+    @Test
+    public void HomePageOpenSuccessfully(){
+
+    }
+}
